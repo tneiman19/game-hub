ab9e594113ca0351079d68f1ef05b1f0b65a8a8e
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-06-27 20:07:07 -0400

    Form hooks and validation using react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,39 +1,47 @@
-import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: '',
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          value={person.name}
-          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          {...register("name", { required: true, minLength: 3, maxLength: 10 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-warning">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-warning">
+            The name must be at least 3 characters.
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          onChange={(e) =>
-            setPerson({ ...person, age: parseInt(e.target.value) })
-          }
-          value={person.age}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
